87d98f0a8681cfea3642843c4e45ba79b6c18993
Boris Scheiber <scheiberboris@hotmail.com>
2025-05-02 22:22:10 +0200

    final push project stopped

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    // setIsEditing(!isEditing);  // wert umkehren nicht so da asynchron
     setIsEditing(prevState => !prevState);
   }
 
-    let playerName = <span className="player-name">{name}</span>
+  function handleChange(event) {
+    console.log(event.target.tagName);
+    
+    setPlayerName(event.target.value);
+  }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
     // let buttonText = "Edit"; // so oder ternary operator unten
   
     if (isEditing) {
-        playerName = <input type="text" value={name} required />
+        editablePlayerName = <input type="text" value={playerName} onChange={handleChange} required />
         // buttonText = "Save";
     }
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
